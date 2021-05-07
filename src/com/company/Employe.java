211eec60d389e2b636ff79c7d6ba58983fe2723f
AlbinDavis <lbndvs@gmail.com>
2021-05-07 10:34:08 +0530

    class and objects

---
+++ src/com/company/Employe.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Employe {
+    private String ename;
+    private String eno;
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getEno() {
+        return eno;
+    }
+
+    public void setEno(String eno) {
+        this.eno = eno;
+    }
+    public void display(){
+        System.out.println("Employeee");
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,11 @@ import java.util.Scanner;
 import java.io.*;
 public class Main {
     public static void main(String[] args) {
-        int n;
-        Scanner s=new Scanner(System.in);
-        System.out.println("Enter the number to check");
-        n=s.nextInt();
-        if(n%2==0){
-            System.out.println("Number is even");
-        }
-        else{
-            System.out.print("Number is odd");
-        }
+Employe emp1=new Employe();
+emp1.setEname("alan");
+emp1.setEno("123");
+emp1.display();
+
+
     }
 }
