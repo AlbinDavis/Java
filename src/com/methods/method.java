3c123a24cdecb73bcc77ced53e349f03174c9aad
AlbinDavis <lbndvs@gmail.com>
2021-05-10 23:37:03 +0530

    array

---
+++ src/com/methods/method.java
@@ -0,0 +1,13 @@
+package com.methods;
+
+public class method {
+    static int add(int a , int b){
+        return(a+b);
+    }
+    public static void main(String[] args) {
+        int a=3;
+        int b=5;
+        System.out.println(add(a,b));
+
+    }
+}
