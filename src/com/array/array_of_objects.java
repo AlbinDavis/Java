a585e1cb0753fc6b051e588deb73f70cb32d6c4e
AlbinDavis <lbndvs@gmail.com>
2021-05-12 08:37:29 +0530

    next

--- src/com/array/array_of_objects.java
+++ src/com/array/array_of_objects.java
@@ -1,28 +1,33 @@
 package com.array;
 import java.util.Scanner;
 
-class Student{
-
-    public int rollno;
-    public String name;
-    Student(int rollno,String name){
-        this.name=name ;
-        this.rollno=rollno;
-    }
-}
-
 public class array_of_objects {
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
-        Student[] arr = new Student[2];
+        s.nextLine();
+        Student[] arr = new Student[n];
         for(int i=0;i<n;i++) {
-            String name=s.nextLine();
             int rollno=s.nextInt();
+            s.nextLine();
+            String name=s.nextLine();
+
             arr[i] = new Student(rollno,name);
+        }System.out.println("Student data in student:\n");
+        for(int i=0;i<n;i++) {
+            System.out.println(+arr[i].rollno+" "+arr[i].name);
         }
-        for(int i=0;i<2;i++) {
-            System.out.println("Student data in student: "+arr[i].rollno+" "+arr[i].name);
+    }
+
+    static class Student {
+
+        public int rollno;
+        public String name;
+
+        Student(int rollno, String name) {
+            this.name = name;
+            this.rollno = rollno;
         }
+
     }
 }
---
+++ src/com/loops/Enhanced_for_loop.java
@@ -0,0 +1,10 @@
+package com.loops;
+
+public class Enhanced_for_loop {
+    public static void main(String[] args) {
+    int[] arr ={1,2,3,4,5};
+    for(int i:arr){
+        System.out.println(i);
+    }
+    }
+}
