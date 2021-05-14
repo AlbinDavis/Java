e4570b7a84f6fda782e6595fc71ad496a415c43e
AlbinDavis <lbndvs@gmail.com>
2021-05-14 13:46:43 +0530

    next

---
+++ src/com/constructor/cons.java
@@ -0,0 +1,11 @@
+package com.constructor;
+class Employee{
+    Employee(){
+        System.out.println("Employeee");
+    }
+}
+public class cons {
+    public static void main(String[] args) {
+    Employee e=new Employee();
+    }
+}
---
+++ src/com/constructor/pracons.java
@@ -0,0 +1,10 @@
+package com.constructor;
+class Empl {
+Empl(int roll){
+        System.out.println(roll);
+        }}
+public class pracons {
+    public static void main(String[] args) {
+        Empl e=new Empl(3);
+    }
+}
