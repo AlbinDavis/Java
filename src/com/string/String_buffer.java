77748357f2946286538c69a08b50430d3703ca64
AlbinDavis <lbndvs@gmail.com>
2021-05-28 18:32:18 +0530

    next

---
+++ src/com/string/String_buffer.java
@@ -0,0 +1,9 @@
+package com.string;
+
+public class String_buffer {
+    public static void main(String[] args) {
+        StringBuffer sb =new StringBuffer("java");
+        sb.append(" hello");
+        System.out.println(sb);
+    }
+}
