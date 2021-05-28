d5f6c6c2348e8d9789ac280e46e37d8f296d643a
AlbinDavis <lbndvs@gmail.com>
2021-05-28 18:40:22 +0530

    next

--- src/com/string/String_buffer.java
+++ src/com/string/String_buffer.java
@@ -5,5 +5,13 @@ public class String_buffer {
         StringBuffer sb =new StringBuffer("java");
         sb.append(" hello");
         System.out.println(sb);
+        System.out.println(sb.reverse());
+        StringBuffer sb1 =new StringBuffer("Hello java");
+        sb1.delete(0,6);
+        System.out.println(sb1);
+        sb1.insert(2,"Hello");
+        System.out.println(sb1);
+        sb1.replace(2,9,"va");
+        System.out.println(sb1.capacity());
     }
 }
