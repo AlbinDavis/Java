3231ad68e60efa916fd368a9a5b9ce9fc98cb211
AlbinDavis <lbndvs@gmail.com>
2021-06-11 22:12:03 +0530

    next

--- src/com/Inheritance/inheritance.java
+++ src/com/Inheritance/inheritance.java
@@ -1,11 +1,13 @@
 package com.Inheritance;
 class base {
     public void baseclass() {
-        System.out.println("hello");
+        System.out.println("base class");
     }
 }
     class Course extends base {
-
+        public void print() {
+            System.out.println("child class");
+        }
     }
 
 public class inheritance {
@@ -13,6 +15,7 @@ public class inheritance {
     public static void main(String[] args) {
     Course java =new Course();
     java.baseclass();
+    java.print();
 
     }
 }
