6e623e6f5f4c8da47134baa94f13f4aaac7d5d6e
AlbinDavis <lbndvs@gmail.com>
2021-06-12 18:52:49 +0530

    next

--- src/com/interfaces/interface1.java
+++ src/com/interfaces/interface1.java
@@ -2,6 +2,7 @@ package com.interfaces;
 //difference btwn interface and abstract is that in abstract we can have
 // signature of a function and we can also define the
 //function too but in interface we can only have the signature of the function no definition
+//whenever we implement the methods in interface we should use public infront of the method
 interface first{
     void a();
     void b();
