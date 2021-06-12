04c1aa0982e3e5320685ff360e4bad39c8275106
AlbinDavis <lbndvs@gmail.com>
2021-06-12 14:11:25 +0530

    next

---
+++ src/com/Inheritance/instant_initializing_block.java
@@ -0,0 +1,17 @@
+package com.Inheritance;
+class C{
+    {
+    System.out.println("course");}
+}
+class D extends C{
+    {//it is the instant initializing block which make a constructor by default and call super() function
+
+    System.out.println("java course");}
+}
+class instant_initializing_block {
+    public static void main(String[] args) {
+        new D();
+    }
+
+
+}
