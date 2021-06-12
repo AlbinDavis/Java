6faf1685bd855a4f624f6fbbf9063f692005d603
AlbinDavis <lbndvs@gmail.com>
2021-06-12 14:53:12 +0530

    next

---
+++ src/com/polymorphism/poly2.java
@@ -0,0 +1,16 @@
+package com.polymorphism;
+class B{
+    void show(int x){
+        System.out.println("x="+x);
+    }
+    void show(double s){
+        System.out.println(s);
+    }
+}
+public class poly2 {
+    public static void main(String[] args) {
+        B a=new B();
+        a.show('p');//it is type casted and convert to integer
+        a.show(66.789);
+    }
+}
