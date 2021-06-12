5835966a28d5963c86c2faaad89c19db540e70f2
AlbinDavis <lbndvs@gmail.com>
2021-06-12 23:03:22 +0530

    next

--- src/com/Enums/methods_enum.java
+++ src/com/Enums/methods_enum.java
@@ -3,8 +3,7 @@ package com.Enums;
 public class methods_enum {
     public static void main(String[] args) {
 
-        CovidVaccines arr[] = CovidVaccines.values();
-        for(CovidVaccines i :arr)
+        for(CovidVaccines i :CovidVaccines.values())
         {
             System.out.println(i + " at index "
                     + i.ordinal());
