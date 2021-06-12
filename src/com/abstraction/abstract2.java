3c684fc416f4f1e4adffeb9353ee06e8ae2748df
AlbinDavis <lbndvs@gmail.com>
2021-06-12 17:34:49 +0530

    next

---
+++ src/com/abstraction/abstract2.java
@@ -0,0 +1,17 @@
+package com.abstraction;
+abstract class base{
+    //void f();    we cant write like this either we should use the abstract keyword or define the function
+}
+class derived extends base{
+    void f() {//signature of method
+        System.out.println("function");
+    }
+}
+public class abstract2 {
+    public static void main(String[] args) {
+        derived d=new derived();
+        d.f();
+
+
+    }
+}
