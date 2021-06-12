ccc10077f0db53df644039946a501be5e1b126f3
AlbinDavis <lbndvs@gmail.com>
2021-06-12 14:47:26 +0530

    next

--- src/com/polymorphism/Poly1.java
+++ src/com/polymorphism/Poly1.java
@@ -3,10 +3,14 @@ class A{
 void show(int x , int y){
     System.out.println("x="+x+"\ny="+y);
 }
-void show(){
-
+void show(String s){
+    System.out.println(s);
 }
 }
 public class Poly1 {
-
+    public static void main(String[] args) {
+        A a=new A();
+        a.show("java course");
+        a.show(1,2);
+    }
 }
