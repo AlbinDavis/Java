e9e3faabaa92e72b9232a02bd831d5f9ab1ecef2
AlbinDavis <lbndvs@gmail.com>
2021-06-13 12:51:56 +0530

    next

---
+++ src/com/collection_framework/sorting_arraylist_elements.java
@@ -0,0 +1,22 @@
+package com.collection_framework;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class sorting_arraylist_elements {
+    public static void main(String[] args) {
+        ArrayList<Integer> li = new ArrayList<Integer>();
+        li.add(5);
+        li.add(3);
+        li.add(6);
+        li.add(1);
+        System.out.println("Before sorting");
+        for(Integer i :li)
+            System.out.println(i);
+        System.out.println("After sorting");
+        Collections.sort(li);
+        for(Integer i :li)
+            System.out.println(i);
+    }
+
+}
