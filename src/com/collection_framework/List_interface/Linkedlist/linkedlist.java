2ad1c583afd61c986e941662d71beb8bb6927c24
AlbinDavis <lbndvs@gmail.com>
2021-06-13 13:41:18 +0530

    next

--- src/com/collection_framework/List_interface/Linkedlist/linkedlist.java
+++ src/com/collection_framework/List_interface/Linkedlist/linkedlist.java
@@ -1,4 +1,17 @@
 package com.collection_framework.List_interface.Linkedlist;
 
+import java.util.*;
+
 public class linkedlist {
+    public static void main(String[] args) {
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println("Total no of elements in list="+ cars.size());
+        Iterator<String> i = cars.iterator();
+        while(i.hasNext())
+            System.out.println(i.next());
+    }
 }
