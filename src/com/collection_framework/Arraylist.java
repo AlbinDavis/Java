5fb3a3118cdbe7ae7a41abb6cf92548b05041c83
AlbinDavis <lbndvs@gmail.com>
2021-06-13 12:20:14 +0530

    next

---
+++ src/com/collection_framework/Arraylist.java
@@ -0,0 +1,23 @@
+package com.collection_framework;
+import java.util.*;
+public class Arraylist {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println("size if list=" + cars.size());
+        System.out.println(cars);
+        System.out.println(cars.get(0));
+        System.out.println("\nUsing for each loop\n");
+        for (String i : cars)
+            System.out.println(i);
+        System.out.println("\nUsing iterator\n");
+        Iterator<String> i = cars.iterator();
+        while(i.hasNext())
+            System.out.println(i.next());
+
+    }
+
+}
