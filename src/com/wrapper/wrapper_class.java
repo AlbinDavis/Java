2cd685ff00c4da3f09821405914dc5f8432856f4
Albin Davis <lbndvs@gmail.com>
2022-06-17 08:16:21 +0530

    wrapper class changes

--- src/com/wrapper/wrapper_class.java
+++ src/com/wrapper/wrapper_class.java
@@ -2,17 +2,24 @@ package com.wrapper;
 
 public class wrapper_class {
     public static void main(String[] args) {
+
+        //AutoBoxing
         int a=10;
-        Integer wrap=Integer.valueOf(a);
-        System.out.println(wrap);
-        //auto boxing
-        int z=wrap;
-        System.out.println(z);
-        //primitive to wrapper
-        int y =wrap.intValue();
+        Integer b= a;
+        System.out.println(b);
+
+        //auto Unboxing
+        int c=b;
+        System.out.println(c);
+
+
+        int y = 10;
+        //wrapping
+        Integer x = new Integer(y);
         System.out.println(y);
+
         //auto unboxing
-        int s=y;
+        int s=x;
         System.out.println(s);
 
     }
