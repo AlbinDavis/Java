5e7f1c6b4eaf0f879497712eaf7c0946dad706f4
Albin Davis <lbndvs@gmail.com>
2022-06-23 09:42:19 +0530

    new programs

---
+++ src/com/programs/LenghtOfLongestSubstring.java
@@ -0,0 +1,22 @@
+package com.programs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LenghtOfLongestSubstring {
+    public static void main(String[] args) {
+        String s="geeksforgeeks";
+        Map<Character,Integer> map = new HashMap();
+        int start=0;
+        int m=0;
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                start=Math.max(map.get(s.charAt(i))+1,start);
+            }
+            m=Math.max(m,i-start+1);
+            map.put(s.charAt(i),i);
+        }
+        System.out.println(m);
+
+    }
+}
