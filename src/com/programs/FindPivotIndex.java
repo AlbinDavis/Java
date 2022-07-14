d1c452eeaa9233b51f3f143d549559a823ef16fc
Albin Davis <lbndvs@gmail.com>
2022-07-14 08:48:51 +0530

    FindPivotIndex

---
+++ src/com/programs/FindPivotIndex.java
@@ -0,0 +1,17 @@
+package com.programs;
+
+public class FindPivotIndex {
+    public static void main(String[] args) {
+        int[] nums = {1,7,3,6,5,6};
+        int sum = 0, leftsum = 0;
+        for (int x: nums) sum += x;
+        for (int i = 0; i < nums.length; ++i) {
+            if (leftsum == sum - leftsum - nums[i]){
+                System.out.println(i);
+                System.exit(0);
+            }
+            leftsum += nums[i];
+        }
+        System.out.println("not present");
+    }
+}
