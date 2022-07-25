d778969c342077b48ac15e1c70142c1f125dc4db
Albin Davis <lbndvs@gmail.com>
2022-07-25 08:05:26 +0530

    FactorsOfNumber FourDivisors

---
+++ src/com/programs/FactorsOfNumber.java
@@ -0,0 +1,24 @@
+package com.programs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FactorsOfNumber {
+    public static void main(String[] args) {
+        int n=64;
+        List<Integer> factors = new ArrayList<>();
+
+        for(int i=1;i<Math.sqrt(n);i++){
+            if(n%i==0){
+                if(n/i!=i){
+                    factors.add(i);
+                    factors.add(n/i);
+                }
+                else{
+                    factors.add(i);
+                }
+            }
+        }
+        System.out.println(factors);
+    }
+}
---
+++ src/com/programs/GCD.java
@@ -0,0 +1,16 @@
+package com.programs;
+
+public class GCD {
+    public static void main(String[] args) {
+        System.out.println(gcd(96,128));
+
+    }
+    private static int gcd(int a,int b){
+        if(a==0){
+            return b;
+        }
+        else{
+            return gcd(b%a,a);
+        }
+    }
+}
