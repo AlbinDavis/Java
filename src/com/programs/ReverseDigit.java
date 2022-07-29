4c6ec62a98b0a267bc2cb6560c2b16f587c3ded3
Albin Davis <lbndvs@gmail.com>
2022-07-29 20:00:56 +0530

    ReverseDigit

---
+++ src/com/programs/ReverseDigit.java
@@ -0,0 +1,18 @@
+package com.programs;
+
+public class ReverseDigit {
+    public static void main(String[] args) {
+        int n=43261596;
+        int result = 0;
+        for(int i=0;i<32;i++){
+            result+=n & 1;
+            //OR result^=n & 1;
+            n>>=1;
+            if(i<31){
+                result<<=1;
+            }
+
+        }
+        System.out.println(result);
+    }
+}
