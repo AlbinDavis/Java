ab04a8c74d073bc1eb85e35273b5b1b3a1dfe300
Albin Davis <lbndvs@gmail.com>
2022-07-29 19:39:08 +0530

    NextLargerElement

---
+++ src/com/programs/NextLargerElement.java
@@ -0,0 +1,26 @@
+package com.programs;
+
+import java.util.*;
+
+public class NextLargerElement {
+    public static void main(String[] args) {
+        long[] arr = {6, 8 ,0, 1 ,3};
+        long[] ans = new long[arr.length];
+        int j=arr.length-1;
+        Stack<Long> stack = new Stack<>();
+        for(int i=arr.length-1;i>=0;i--){
+            while(!stack.isEmpty() && stack.peek()<arr[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                ans[j]=-1;
+            }
+            else{
+                ans[j]=stack.peek();
+            }
+            stack.push(arr[i]);
+            j--;
+        }
+        System.out.println(Arrays.toString(ans));
+    }
+}
