666caf95d86ffa74cd2c29682fdef5a415309e11
Albin Davis <lbndvs@gmail.com>
2022-08-03 07:29:34 +0530

    isSubSequence

--- src/com/programs/isSubSequence.java
+++ src/com/programs/isSubSequence.java
@@ -1,9 +1,11 @@
 package com.programs;
 
+import java.util.Stack;
+
 public class isSubSequence {
     public static void main(String[] args) {
         String s = "abc", t = "ahbgdc";
-        System.out.println(isSubsequence(s,t));
+        System.out.println(isSubsequenceUsingStack(s,t));
     }
     public static boolean isSubsequence(String s, String t) {
         if(s.length()==0){
@@ -16,6 +18,23 @@ public class isSubSequence {
             }
             if(j==s.length())
                 return true;
+        }
+        return false;
+    }
+    private static boolean isSubsequenceUsingStack(String s, String t){
+        if(s.length()==0){
+            return true;
+        }
+        Stack<Character> stack = new Stack<>();
+        for(char i: s.toCharArray())
+            stack.add(i);
+        for(int i=t.length()-1;i>=0;i--){
+            if(stack.peek()==t.charAt(i)){
+                stack.pop();
+            }
+            if(stack.size()==0){
+                return true;
+            }
         }
         return false;
     }
