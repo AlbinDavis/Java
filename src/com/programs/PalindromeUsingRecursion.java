b603665101f758bbed15afec032f5f6fb56d8d2b
Albin Davis <lbndvs@gmail.com>
2022-08-25 17:07:02 +0530

    PalindromeUsingRecursion

--- src/com/programs/PalindromeUsingRecursion.java
+++ src/com/programs/PalindromeUsingRecursion.java
@@ -3,12 +3,12 @@ package com.programs;
 public class PalindromeUsingRecursion {
     public static void main(String[] args) {
 
-        String str = "geeg";
+        String str = "geekg";
         System.out.println(isPalindrome(str,0,str.length()-1));
     }
 
     private static boolean isPalindrome(String str,int start,int end) {
-        if(start>=end){
+        if(start>end){
             return true;
         }
         if(str.charAt(start)==str.charAt(end)){
