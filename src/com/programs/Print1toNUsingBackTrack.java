f710a9466c42e20d3f4947fc2133466f3572aaa7
Albin Davis <lbndvs@gmail.com>
2022-09-14 18:13:13 +0530

    Print1toNUsingBackTrack

---
+++ src/com/programs/Print1toNUsingBackTrack.java
@@ -0,0 +1,16 @@
+package com.programs;
+
+public class Print1toNUsingBackTrack {
+    public static void main(String[] args) {
+        printN(5);
+    }
+
+    private static void printN(int i) {
+        if(i<1)
+            return;
+        printN(i-1);
+        System.out.println(i);
+        
+    }
+
+}
