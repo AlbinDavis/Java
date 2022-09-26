b1dfca20bb45b66fec502b973b5ad8f0c9f74387
Albin Davis <lbndvs@gmail.com>
2022-09-26 09:48:57 +0530

    SumOfNumbersWithoutAdditionOperator

---
+++ src/com/programs/SumOfNumbersWithoutAdditionOperator.java
@@ -0,0 +1,24 @@
+package com.programs;
+
+public class SumOfNumbersWithoutAdditionOperator {
+    public static void main(String[] args) {
+        //System.out.println(add(2,3));
+        int a=2;
+        int b=3;
+        int carry=0;
+        while(b!=0){
+            int temp=a;
+            a=a^b;
+            b=(temp&b)<<1;
+        }
+        System.out.println(a);
+    }
+
+
+    public static int add(int a,int b) {
+        if(b==0){
+            return a;
+        }
+        return add(a^b,(a&b)<<1);
+    }
+}
