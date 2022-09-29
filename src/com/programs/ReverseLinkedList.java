51634d49ed108f0fb1c56ae16d1215d47b509734
Albin Davis <lbndvs@gmail.com>
2022-09-29 21:30:01 +0530

    ReverseLinkedList

---
+++ src/com/programs/ReverseLinkedList.java
@@ -0,0 +1,22 @@
+package com.programs;
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        ListNode node= new ListNode();
+        node.addAtTail(1);
+        node.addAtTail(2);
+        node.addAtTail(3);
+        node.addAtTail(4);
+        ListNode head=node.head,prev=null,curr=head;
+        while(curr!=null){
+            head = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = head;
+        }
+        while(prev!=null){
+            System.out.println(prev.val);
+            prev= prev.next;
+        }
+    }
+}
