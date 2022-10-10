a63a1fde6652313a06d9b2f5a691c20a16f9c408
Albin Davis <lbndvs@gmail.com>
2022-10-10 20:01:03 +0530

    TreeBFS

---
+++ src/com/programs/TreeBFS.java
@@ -0,0 +1,28 @@
+package com.programs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+//Also known as level order traversal.
+public class TreeBFS {
+    public static void main(String[] args) {
+        TreeNode<Integer> root = new TreeNode<>(1);
+        root.left=new TreeNode<>(2,new TreeNode<>(4),new TreeNode<>(5));
+        root.right = new TreeNode<>(3,new TreeNode<>(6),new TreeNode<>(7));
+        BFS(root);
+
+    }
+    private static void BFS(TreeNode<Integer> root){
+        Queue<TreeNode<Integer>> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            TreeNode<Integer> curr = queue.poll();
+            System.out.println(curr.val);
+            if(curr.left != null)
+                queue.add(curr.left);
+            if(curr.right != null)
+                queue.add(curr.right);
+        }
+    }
+
+}
