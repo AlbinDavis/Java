868d1adedbb8ad9fa952eef3afaa0fd21d53c555
Albin Davis <lbndvs@gmail.com>
2022-10-17 21:27:00 +0530

    CreateBinaryTree

---
+++ src/com/programs/CreateBinaryTree.java
@@ -0,0 +1,17 @@
+package com.programs;
+
+import java.util.List;
+
+public class CreateBinaryTree{
+     static TreeNode<Integer> createTree(List<Integer> list, int index) {
+        TreeNode<Integer> root = null;
+        if (index < list.size() && list.get(index)!=null) {
+                root = new TreeNode<>(list.get(index));
+            if(root.val != null) {
+                root.left = createTree(list, index * 2 + 1);
+                root.right = createTree(list, index * 2 + 2);
+            }
+        }
+        return root;
+    }
+}
