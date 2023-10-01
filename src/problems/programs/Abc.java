1ee83fb21da9d2f8223587480130272ab988b987
Albin Davis <lbndvs@gmail.com>
2023-10-01 18:40:39 +0530

    ones

--- src/problems/programs/Abc.java
+++ src/problems/programs/Abc.java
@@ -2,6 +2,6 @@ package problems.programs;
 
 public class Abc {
     public static void main(String[] args) {
-        System.out.println("adfss fczbfxb");
+        System.out.println("adfss sgdf ssfczbfxb");
     }
 }
